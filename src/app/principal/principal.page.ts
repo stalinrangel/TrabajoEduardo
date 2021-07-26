75892310fa23a1fc92879fb4e84e1d453b47cf7c
stalinrangel <e.stalinrangel@gmail.com>
2021-07-26 17:44:07 -0430

    phptp

--- src/app/principal/principal.page.ts
+++ src/app/principal/principal.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.page.html',
   styleUrls: ['./principal.page.scss'],
 })
+
+
 export class PrincipalPage implements OnInit {
 
-  constructor() { }
+  constructor(public photoService: PhotoService) { }
 
   ngOnInit() {
   }
 
+  addPhotoToGallery() {
+    
+    this.photoService.addNewToGallery();
+  }
+
+  get_url(){
+    console.log(this.photoService.photos[0]);
+  }
+  
+
 }
+
+
---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  public photos: Photos[] = [];
+  constructor() { }
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+  
+    this.photos.unshift({
+      filepath: "",
+      webviewPath: capturedPhoto.webPath
+    });
+  }
+
+  public url(){
+    return this.photos;
+  }
+}
+export interface Photos {
+  filepath: string;
+  webviewPath: string;
+}
